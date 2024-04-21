effe5098c2af69bf51c40b061e9e888390a66927
池上ノイ <93964832+IkegamiNoi@users.noreply.github.com>
2024-04-21 23:26:36 +0900

    first_flutterのボタンを追加で弄ったものにコメントを行った

---
+++ test_drive/first_flutter/lib/main.dart
@@ -0,0 +1,67 @@
+import 'package:english_words/english_words.dart';
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return ChangeNotifierProvider(
+      create: (context) => MyAppState(),
+      child: MaterialApp(
+        title: 'Namer App',
+        theme: ThemeData(
+          useMaterial3: true,
+          colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepOrange),
+        ),
+        home: MyHomePage(),
+      ),
+    );
+  }
+}
+
+/// MyAppStateはアプリの状態変化について記述を行っている。
+/// ChangeNotifierによって変更を通知できる。
+class MyAppState extends ChangeNotifier {
+  var current = WordPair.random();
+
+  /// getNextはWordPair.random()して、notifyListeners()
+  /// はChangeNotifierのメソッドであり、これによって変更を通知出来る。
+  // ↓ Add this.
+  void getNext() {
+    current = WordPair.random();
+    notifyListeners();
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    var appState = context.watch<MyAppState>();
+
+    return Scaffold(
+      body: Column(
+        children: [
+          Text('A random AWESOME idea:'),
+          Text(appState.current.asLowerCase),
+
+          // ↓ Add this.
+          ElevatedButton(
+            onPressed: () {
+              //print('button pressed!');
+              /// MyAppStateをappStateで宣言しているのでgetNext()を呼べている。
+              /// これによってボタンを押すたびにワードが変更される。
+              appState.getNext();  // ← This instead of print().
+            },
+            child: Text('Next'),
+          ),
+        ],
+      ),
+    );
+  }
+}
