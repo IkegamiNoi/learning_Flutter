7ebf678c9c2888cc8d51f7ca5acc4ac852079b2e
池上ノイ <93964832+IkegamiNoi@users.noreply.github.com>
2024-05-02 16:04:54 +0900

    5. アプリを美しくする Card内のテキストにスタイルを設定し、アプリのテーマに合わせられるようにした。

--- test_drive/first_flutter/lib/main.dart
+++ test_drive/first_flutter/lib/main.dart
@@ -17,9 +17,9 @@ class MyApp extends StatelessWidget {
         title: 'Namer App',
         theme: ThemeData(
           useMaterial3: true,
-          // アプリのテーマカラーを決める。変更するとアニメーションで色が変化する。
-          // UI を状態から状態へジャンプさせるのではなく、値と値の間を滑らかに補間します。
-          // カードの下の浮き上がりボタンの色も変化しています。これが、値をハードコードするのではなく、アプリ全体が対象の Theme を使用するメリットです。
+          /// アプリのテーマカラーを決める。変更するとアニメーションで色が変化する。
+          /// UI を状態から状態へジャンプさせるのではなく、値と値の間を滑らかに補間します。
+          /// カードの下の浮き上がりボタンの色も変化しています。これが、値をハードコードするのではなく、アプリ全体が対象の Theme を使用するメリットです。
           colorScheme: ColorScheme.fromSeed(seedColor: Colors.green),
         ),
         home: MyHomePage(),
@@ -80,16 +80,28 @@ class BigCard extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    // アプリの現在のテーマをリクエスト
-    // MyApp までスクロールし、そこで ColorScheme のシード色を変更すると、この色とアプリ全体のカラーパターンを変更できます。
+    /// アプリの現在のテーマをリクエスト
+    /// MyApp までスクロールし、そこで ColorScheme のシード色を変更すると、この色とアプリ全体のカラーパターンを変更できます。
     final theme = Theme.of(context);       // ← Add this.
+    // ↓ Add this.
+    /// theme.textTheme, を使用して、アプリのフォントテーマにアクセスします。このクラスには、bodyMedium（中サイズの標準テキスト用）、caption（画像のキャプション用）、headlineLarge（大きな見出し用）などのメンバーがあります。
+    /// displayMedium プロパティは、ディスプレイ テキスト用の大きなスタイルです。ここで「ディスプレイ」という単語は、ディスプレイ書体（見出し書体）などのタイポグラフィでの意味で使用されています。displayMedium のドキュメントには「ディスプレイ スタイルは短く、重要なテキストにのみ使用します」と書かれており、まさに我々のユースケースです。
+    /// displayMedium の copyWith() を呼び出すと、定義した変更が反映されたテキスト スタイルのコピーが返されます。この場合は、テキストの色のみを変更しています。
+    /// copyWithの()内にカーソルを合わせてCtrl+Shift+Space（Win / Linux）か Cmd+Shift+Space（Mac) で変更可能なプロパティを確認できる。
+    final style = theme.textTheme.displayMedium!.copyWith(
+      /// theme.colorScheme.primary 以外にも、.secondary や .surface など無数にあります。これらの色のすべてに onPrimary と同等のものがあります。
+      color: theme.colorScheme.onPrimary,
+    );
 
     return Card(
-      // カードの色をテーマの colorScheme プロパティと同じになるよう定義しています。カラーパターンには多数の色が含まれていますが、primary がこのアプリの最も目立つ特徴的な色です。
+      /// カードの色をテーマの colorScheme プロパティと同じになるよう定義しています。カラーパターンには多数の色が含まれていますが、primary がこのアプリの最も目立つ特徴的な色です。
+      /// Card ウィジェットにも、他に変更できるパラメータがあります。たとえば、elevation パラメータの値を増やせば、カードの影を大きくできます。
       color: theme.colorScheme.primary,    // ← And also this.
       child: Padding(
         padding: const EdgeInsets.all(20.0),
-        child: Text(pair.asLowerCase),
+        // ↓ Change this line.
+        /// buildで定義したstyleを追加して文字のサイズ、色を変化させている。
+        child: Text(pair.asLowerCase, style: style),
       ),
     );
   }
