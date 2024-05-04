03d05d1c87e1bd8aa5b2e4530cd4f2f91def2166
池上ノイ <93964832+IkegamiNoi@users.noreply.github.com>
2024-05-04 19:14:39 +0900

    7. ナビゲーション レールを追加する 「FavoritesPage」が表示されるように修正を行った。 「FavoritesPage」を作成し、「Placeholder 」と入れ替えた。 ※「Placeholder 」は作成中のページとしてSDKから提供されている。

--- test_drive/first_flutter/lib/main.dart
+++ test_drive/first_flutter/lib/main.dart
@@ -79,7 +79,7 @@ class _MyHomePageState extends State<MyHomePage> {
           break;
         case 1:
           /// FavoritesPage はまだないので、Placeholder という、配置した場所に十字が入った四角形を描画して、その部分の UI が未完成であることを示す便利なウィジェットを使用します。
-          page = Placeholder();
+          page = FavoritesPage();
           break;
         default:
           /// selectedIndex が 0 でも 1 でもない場合にエラーをスローするようにしています。これによって以降のバグを防ぐことができます。
@@ -232,6 +232,34 @@ class BigCard extends StatelessWidget {
           semanticsLabel: "${pair.first} ${pair.second}",
         ),
       ),
+    );
+  }
+}
+
+class FavoritesPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    var appState = context.watch<MyAppState>();
+
+        if (appState.favorites.isEmpty) {
+      return Center(
+        child: Text('No favorites yet.'),
+      );
+    }
+
+    return ListView(
+      children: [
+        Padding(
+          padding: const EdgeInsets.all(20),
+          child: Text('You have '
+              '${appState.favorites.length} favorites:'),
+        ),
+        for (var pair in appState.favorites)
+          ListTile(
+            leading: Icon(Icons.favorite),
+            title: Text(pair.asLowerCase),
+        ),
+      ],
     );
   }
 }
