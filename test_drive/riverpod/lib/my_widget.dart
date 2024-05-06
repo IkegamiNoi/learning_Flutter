dc44b4182816fb30961ee44d83fc50231f42eb1a
池上ノイ <93964832+IkegamiNoi@users.noreply.github.com>
2024-05-06 16:10:20 +0900

    以下のエラーが出ていたので修正した
    The class 'Consumer' shouldn't be extended, mixed in, or implemented because it's sealed.
    Try composing instead of inheriting, or refer to the documentation of 'Consumer' for more information.dartsubtype_of_sealed_class

--- test_drive/riverpod/lib/my_widget.dart
+++ test_drive/riverpod/lib/my_widget.dart
@@ -1,13 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:folder_manager/notifier.dart';
 
-class MyWidget extends Consumer {
+class MyWidget extends ConsumerWidget {
   const MyWidget({super.key});
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
 
     final s1 = ref.watch(s1NotifierProvider);
-    return Text(s1);
+    return Text('$s1');
   }
 }
---
+++ test_drive/riverpod/lib/notifier.g.dart
@@ -0,0 +1,27 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'notifier.dart';
+
+// **************************************************************************
+// RiverpodGenerator
+// **************************************************************************
+
+String _$s1NotifierHash() => r'50807122c169d62988d5d4499fe8db162e4931b5';
+
+/// 自動生成する名称として自動生成元のファイル名と拡張子の間に「.g」と入れたものをここに記述する
+///
+/// Copied from [S1Notifier].
+@ProviderFor(S1Notifier)
+final s1NotifierProvider =
+    AutoDisposeNotifierProvider<S1Notifier, int>.internal(
+  S1Notifier.new,
+  name: r's1NotifierProvider',
+  debugGetCreateSourceHash:
+      const bool.fromEnvironment('dart.vm.product') ? null : _$s1NotifierHash,
+  dependencies: null,
+  allTransitiveDependencies: null,
+);
+
+typedef _$S1Notifier = AutoDisposeNotifier<int>;
+// ignore_for_file: type=lint
+// ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member, invalid_use_of_visible_for_testing_member
