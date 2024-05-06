9e4527d5ad91354154d9cf67bddecf9b67a18755
池上ノイ <93964832+IkegamiNoi@users.noreply.github.com>
2024-05-06 17:06:07 +0900

    コメントを記述。 使い分け方のドキュメントを作成。 listemの使い方例を作成。

--- test_drive/riverpod/lib/my_widget.dart
+++ test_drive/riverpod/lib/my_widget.dart
@@ -2,16 +2,47 @@ import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:folder_manager/notifier.dart';
 
+/// notifierの以下3つの使い分け
+/// ・watch   見続ける
+/// ・listen  耳を澄まし続ける
+/// ・read    読み取る
+/// `watch`と`listen`は`~続ける`となっている。
+/// これは関数スコープの外まで影響を残すという意味。
+/// `read`はこれを使ったときしか変更を確認できない。
+/// 
+/// `watch`と`listen`は`ref`のある`build`のスコープ範囲内で使用する。
+/// なお`build`のスコープ範囲内でも別のスコープで括られている範囲で使用することはできない。
+/// 例としてはonPressed: () {}内では使えない。
+/// この中では`read`を使用する。
+/// 
+/// `watch`と`listen`の使い分け
+/// `watch`は変更があったらその表示場所を変更する。
+/// `listen`は変更があったら命令を実行できる。
+
 class MyWidget extends ConsumerWidget {
   const MyWidget({super.key});
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-
+    /// buildの中で状態の変化を`watch`で監視
     final s1 = ref.watch(s1NotifierProvider);
+    
+    /// buildの中で状態の変化を`listen`で監視して変化があれば、命令を出す。
+    ref.listen(
+      s1NotifierProvider, 
+      (previous, next) { 
+        // スナックバーを表示する
+        ScaffoldMessenger.of(context).showSnackBar(
+          SnackBar(
+            content: Text('$previous から $next へ変化しました'),
+          ),
+        );
+      },
+    );
 
     final button = ElevatedButton(
       onPressed: () {
+        /// 画面を触ったら(ここではボタン)Notifierを`read`で読み込み
         final notifier = ref.read(s1NotifierProvider.notifier);
         notifier.updateState();
       },
